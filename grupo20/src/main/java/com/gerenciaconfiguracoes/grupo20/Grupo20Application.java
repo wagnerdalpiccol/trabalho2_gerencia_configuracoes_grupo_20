b6660664fb36a2ae589fd2d386b12c99b3cea9fe
Wagner Dal ` Piccol <wdpiccol2005@gmail.com>
2025-10-01 20:16:35 -0300

    chore: estrutura inicial do projeto spring boot, flyway e postgreSQL

---
+++ grupo20/src/main/java/com/gerenciaconfiguracoes/grupo20/Grupo20Application.java
@@ -0,0 +1,13 @@
+package com.gerenciaconfiguracoes.grupo20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo20Application.class, args);
+	}
+
+}
---
+++ grupo20/src/test/java/com/gerenciaconfiguracoes/grupo20/Grupo20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciaconfiguracoes.grupo20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
